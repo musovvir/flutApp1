09490929e122d90c03f98adacea869da667651b9
musovvir <mr.muzagov@mail.ru>
2022-01-13 10:09:37 +0300

    addCommit

---
+++ lib/widgets/user_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text('Texdgst'),
+      ),
+    );
+  }
+}
